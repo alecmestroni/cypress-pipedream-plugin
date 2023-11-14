4295d077997dbe79e292c7573c4b04c25f052981
Alec Mestroni <alec.mestroni@infocert.it>
2023-11-14 11:56:21 +0100

    adds .d.ts and .ignore files

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+declare const dir: any;
+declare namespace HEADERS {
+    let Authorization: any;
+}
+declare namespace options {
+    export let url: string;
+    export { HEADERS as headers };
+}
+declare const maxRetries: any;

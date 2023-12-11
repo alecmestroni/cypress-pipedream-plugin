4a819c621deef26dce57768cc79d4106cf04a973
Alec Mestroni <alec.mestroni@infocert.it>
2023-12-11 09:31:32 +0100

    release 1.2.2

--- index.js
+++ index.js
@@ -11,66 +11,49 @@ const fileName = Cypress.env('pipedreamFileName') ? Cypress.env('pipedreamFileNa
 const baseUrl = "https://api.pipedream.com/v1/sources/" + Cypress.env('pipedreamSourceID')
 
 const writeSMS = function (body) {
-    cy.writeFile(dir + fileName, JSON.stringify(body))
+    cy.writeFile(dir + '/' + fileName, JSON.stringify(body))
+}
+
+const getMessage = (age, count = 0) => {
+    const options = {
+        url: baseUrl + "/event_summaries",
+        headers: HEADERS,
+    }
+    // Loop checking every 1000ms for a max of pipedreamMaxRetries times/seconds
+    if (count === maxRetries) {
+        throw new Error(`CYPRESS-PIPEDREAM-PLUGIN | No message received in ${Cypress.env('pipedreamMaxRetries')}seconds, please check https://pipedream.com/sources/${Cypress.env('pipedreamSourceID')}`)
+    }
+    cy.request(options)
+        .then((res) => {
+            let newMessageAvailable = false
+            let SMSBody
+            if (res.body.data.length !== 0) {
+                newMessageAvailable = true
+                expect(res.status).to.be.eq(200)
+                expect(res.statusText).to.be.eq("OK")
+                expect(res.body.data).to.not.be.empty
+                if (age === 'newest') {
+                    SMSBody = res.body.data[0].event.Body
+                } else if (age === 'oldest') {
+                    SMSBody = res.body.data[res.body.data.length - 1].event.Body
+                }
+                cy.log('📧 ' + SMSBody)
+                writeSMS(SMSBody)
+            }
+            if (newMessageAvailable === false) {
+                cy.wait(1000)
+                getMessage(age, count++)
+            }
+        });
 }
 
 if (Cypress.env('pipedreamBearer') && Cypress.env('pipedreamSourceID')) {
-    Cypress.Commands.add('getFirstMessage', (count = 0) => {
-        const options = {
-            url: baseUrl + "/event_summaries",
-            headers: HEADERS,
-        }
-        // Loop checking every 1000ms for a max of pipedreamMaxRetries times/seconds
-        if (count === maxRetries) {
-            throw new Error(`CYPRESS-PIPEDREAM-PLUGIN | No message received in ${Cypress.env('pipedreamMaxRetries')}seconds, please check ${Cypress.env('pipedreamSourceID')}`)
-        }
-
-        cy.request(options)
-            .then((res) => {
-                let newMessageAvailable = false
-                if (res.body.data.length !== 0) {
-                    newMessageAvailable = true
-                    expect(res.status).to.be.eq(200)
-                    expect(res.statusText).to.be.eq("OK")
-                    expect(res.body.data).to.not.be.empty
-                    const body = res.body.data[res.body.data.length - 1].event.Body
-                    cy.log('📧 ' + body)
-                    writeSMS(body)
-                }
-                if (newMessageAvailable === false) {
-                    cy.wait(1000)
-                    cy.getLastMessage(++count)
-                }
-            });
+    Cypress.Commands.add('getOldestMessage', () => {
+        getMessage('oldest');
     })
 
-    Cypress.Commands.add('getLastMessage', (count = 0) => {
-        const options = {
-            url: baseUrl + "/event_summaries",
-            headers: HEADERS,
-        }
-        // Loop checking every 1000ms for a max of pipedreamMaxRetries times/seconds
-        if (count === maxRetries) {
-            throw new Error(`CYPRESS-PIPEDREAM-PLUGIN | No message received in ${Cypress.env('pipedreamMaxRetries')}seconds, please check ${Cypress.env('pipedreamSourceID')}`)
-        }
-
-        cy.request(options)
-            .then((res) => {
-                let newMessageAvailable = false
-                if (res.body.data.length !== 0) {
-                    newMessageAvailable = true
-                    expect(res.status).to.be.eq(200)
-                    expect(res.statusText).to.be.eq("OK")
-                    expect(res.body.data).to.not.be.empty
-                    const body = res.body.data[0].event.Body
-                    cy.log('📧 ' + body)
-                    writeSMS(body)
-                }
-                if (newMessageAvailable === false) {
-                    cy.wait(1000)
-                    cy.getLastMessage(++count)
-                }
-            });
+    Cypress.Commands.add('getNewestMessage', () => {
+        getMessage('newest');
     })
 
     Cypress.Commands.add('clearMessagesHistory', () => {
@@ -88,7 +71,7 @@ if (Cypress.env('pipedreamBearer') && Cypress.env('pipedreamSourceID')) {
     Cypress.Commands.add('clearHistorySendSmsAndGetSMS', () => {
         cy.clearMessagesHistory()
         cy.get('[data-cy="send-sms"]', { timeout: 120000 }).click() // Command to send the SMS from the frontend
-        cy.getLastMessage()
+        cy.getNewestMessage()
     })
 } else {
     throw new Error(`CYPRESS-PIPEDREAM-PLUGIN | Missing environment variables: env('pipedreamBearer') & env('pipedreamSourceID') needed`)
